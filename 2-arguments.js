4e437340ceee64d74b722a5e38731c684da777de
Fatimah Badmus <fatimahbadmus04@gmail.com>
2025-05-24 21:40:18 +0100

    Printing message depending on the arguement

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,9 @@
-const firstArg= 'School';
+const args = process.argv.slice(2);
 
-if(firstArg===undefined) {
-    console.log("No argument");
-}else {
-    console.log(firstArg);
+if (args[0] === undefined) {
+  console.log("No argument");
+} else if (args[1] === undefined) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
