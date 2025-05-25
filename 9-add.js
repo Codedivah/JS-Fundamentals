d7483b2d0ea26598c63d3c663321399167ea87de
FATIMAH BADMUS <95576634+Codedivah@users.noreply.github.com>
2025-05-25 09:41:42 +0100

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,8 +1,9 @@
-const x = parseInt(process.argv[2]);
-const y = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-if (isNaN(x) || isNaN(y)) {
-  console.log('Not a Number');
-} else {
-  console.log(x + y);
+function add(a, b) {
+  return a + b;
 }
+
+console.log(add(a, b));
+
